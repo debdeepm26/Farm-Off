b626463fdd55d4eb8075bc00100579e50aac4983
GaneshPrasad DK <ganeshd2@illinois.edu>
2023-07-19 10:17:57 -0500

    Start

---
+++ src/main/main.py
@@ -0,0 +1,8 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
