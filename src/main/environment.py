1b00b18bbb754c67b5d3d2de55633e05dc6a3692
debdeepm26 <debdeepm26@gmail.com>
2023-07-19 10:51:08 -0500

    @Deb green screen

--- src/main/environment.py
+++ src/main/environment.py
@@ -1,27 +1,14 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+class Environment:
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    def __init__(self, texture, vertical, horizontal):
+        self.texture = texture
+        self.vertical = vertical
+        self.horizontal = horizontal
 
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
-
-pygame.quit()
+    def render_environment(self):
+        screen = pygame.display.set_mode((1280, 720))
+        return screen.fill(self.texture)
--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,30 @@
 # Example file showing a basic pygame "game loop"
 import pygame
+from environment import Environment
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+
+def main():
+    # pygame setup
+    pygame.init()
+    environment = Environment("purple", 32, 46)
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        environment.render_environment()
+
+        pygame.display.flip()
+        #
+        clock.tick(60)  # limits FPS to 60
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
